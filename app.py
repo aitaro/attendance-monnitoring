0deb7c564114f20d0da8e030a805f5cce8cf7ea9
Aitaro Chaya <aitaro.chaya@gmail.com>
2019-11-09 14:17:36 +0900

    add health check

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask import request
 from main import main
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=["GET", "POST"])
 def root():
     return main(request)
+
+@app.route('/health-check', methods=["GET", "POST"])
+def health_check():
+    return 'ok'
